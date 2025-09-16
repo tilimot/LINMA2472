eb64cfb6c992468ce07f4b5dfda58b4764aa83b4
Benoît Legat <benoit.legat@gmail.com>
2025-09-16 23:11:29 +0200

    Exclude utils.jl

--- pluto-slider-server/generate_pdf.jl
+++ pluto-slider-server/generate_pdf.jl
@@ -3,8 +3,11 @@ import PlutoPDF
 function generate_pdfs(dir)
     for filename in readdir(dir)
         if endswith(filename, ".jl")
+            if filename == "utils.jl"
+                continue
+            end
             @info("Exporting pdf for $(joinpath(dir, filename))")
-            output_path = PlutoPDF.pluto_to_pdf(joinpath(dir, filename))
+            output_path = @time PlutoPDF.pluto_to_pdf(joinpath(dir, filename))
             @info("Exported pdf at $(output_path) : $(isfile(output_path))")
         end
     end
