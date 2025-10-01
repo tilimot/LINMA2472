353a8f5764311d4022f3b6f22a47f1ac2c84616d
Benoît Legat <benoit.legat@gmail.com>
2025-10-01 17:55:50 +0200

    Add skeleton for broadcasting

--- HomeworkAD/reverse_vectorized.jl
+++ HomeworkAD/reverse_vectorized.jl
@@ -4,6 +4,31 @@ mutable struct VectNode
     # TODO
 end
 
+# For `tanh.(X)`
+function Base.broadcasted(op::Function, x::VectNode)
+    error("TODO")
+end
+
+# For `X .* Y`
+function Base.broadcasted(op::Function, x::VectNode, y::VectNode)
+    error("TODO")
+end
+
+# For `X .* Y` where `Y` is a constant
+function Base.broadcasted(op::Function, x::VectNode, y::Union{AbstractArray,Number})
+    error("TODO")
+end
+
+# For `X .* Y` where `X` is a constant
+function Base.broadcasted(op::Function, x::Union{AbstractArray,Number}, y::VectNode)
+    error("TODO")
+end
+
+# For `x .^ 2`
+function Base.broadcasted(::typeof(Base.literal_pow), ::typeof(^), x::VectNode, ::Val{y}) where {y}
+	Base.broadcasted(^, x, y)
+end
+
 # We assume `Flatten` has been defined in the parent module.
 # If this fails, run `include("/path/to/Flatten.jl")` before
 # including this file.
