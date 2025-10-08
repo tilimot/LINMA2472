bd7a0584842ad0225720885a68df8ad1c7876117
Benoît Legat <benoit.legat@gmail.com>
2025-10-08 17:42:47 +0200

    Add test_more_layers and disable ReLU zero tests (#13)

--- LabAD/test/test.jl
+++ LabAD/test/test.jl
@@ -19,28 +19,20 @@ function flatten_gradient(g)
     end
 end
 
-function check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol=1e-8,hessian=false, broken=false)
+function check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol=1e-8, test=true)
     L = loss(loss_function, activation, X, y)
-    try
-        ∇f = Reference_diff(L,deepcopy(w))
-        ∇r = Given_diff(L,deepcopy(w))
-        ∇f = flatten_gradient(∇f)
-        ∇r = flatten_gradient(∇r)
-        if broken 
-            @test_skip isapprox(norm(∇f - ∇r), 0.0; atol=rtol*norm(∇f))
-        else
-            @test isapprox(norm(∇f - ∇r), 0.0; atol=rtol*norm(∇f))
-        end
-    catch e
-        if broken
-            @test_skip "Error"
-        else
-            rethrow(e)
-        end
+    ∇f = Reference_diff(L, deepcopy(w))
+    ∇r = Given_diff(L, deepcopy(w))
+    ∇f = flatten_gradient(∇f)
+    ∇r = flatten_gradient(∇r)
+    if ! test
+        @test_skip "Not doing this test" 
+    else
+        @test isapprox(norm(∇f - ∇r), 0.0; atol = rtol * norm(∇f))
     end
 end
 
-function generate_data(num_data, num_features, num_hidden,classification)
+function generate_data(num_data, num_features, num_hidden, classification)
     X = rand(num_data, num_features)
     y = rand(num_data)
     if classification
@@ -55,7 +47,7 @@ function generate_data(num_data, num_features, num_hidden,classification)
     return X, y, w
 end
 
-function generate_random(classification,hessian)
+function generate_random(classification, hessian)
     if hessian
         num_data = rand(2:10)
         num_features = rand(2:4)
@@ -71,69 +63,75 @@ end
 # -------------------------
 # Generic test runner
 # -------------------------
+# If `zero_test` is `true`, the input of activation layers might be zero.
+# If the activation is `ReLU`, this will test that `Reference_diff` and `Given_diff`
+# give the same gradient even though several gradient are valid which is tricky.
+# Use `zero_test = false` to deactivate them. We don't require you to pass the
+# tests with `zero_test = true` for the project.
 function run_gradient_tests(Reference_diff, Given_diff, loss_function, activation; 
                             name="Test",
                             classification=false,
                             num_repeats=5,
-                            rtol=1e-8, hessian=false, broken=false) #fail for 1e-15
+                            rtol=1e-8, hessian=false,
+                            zero_test=true) 
     @testset "$name" begin
         # 1. Random small tests
         for _ in 1:num_repeats
             # with only positive values
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             X .= abs.(X)
             y .= abs.(y)
             w.components .= [abs.(c) for c in w.components]
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             # with some negative value
-            X, y, w = generate_random(classification,hessian)
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            X, y, w = generate_random(classification, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             # with negative values
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             X .= -abs.(X)
             w.components .= [-abs.(c) for c in w.components]
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
-            X, y, w = generate_random(classification,hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
+            X, y, w = generate_random(classification, hessian)
             X .= -abs.(X)
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
-            X, y, w = generate_random(classification,hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
+            X, y, w = generate_random(classification, hessian)
             w.components .= [-abs.(c) for c in w.components]
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             # edge case scalar
             num_data = 1
             num_hidden = 1
             num_features = 1
-            X, y, w = generate_data(num_data, num_features, num_hidden,classification)
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            X, y, w = generate_data(num_data, num_features, num_hidden, classification)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
         end
     end
 
     @testset "$name with identical labels" begin
         # 2. Random tests with identical labels
         for _ in 1:num_repeats
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             y .= 1.0
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
         end
     end
 
     @testset "$name with small numbers" begin 
         # 3. Random tests with small numbers
         for _ in 1:num_repeats
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             X = X*10^-5
             y = y*10^-5
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
         end
     end
     if !classification # cross_entropy fail with big numbers
         @testset "$name with big numbers" begin 
             # 4. Random tests with big numbers
             for _ in 1:num_repeats
-                X, y, w = generate_random(classification,hessian)
+                X, y, w = generate_random(classification, hessian)
                 X = X*10^3
                 y = y*10^3
-                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             end
         end
     end
@@ -141,10 +139,10 @@ function run_gradient_tests(Reference_diff, Given_diff, loss_function, activatio
         @testset "$name with extreme numbers" begin
             # 5. Random tests with extreme numbers (big for X, small for y)
             for _ in 1:num_repeats 
-                X, y, w = generate_random(classification,hessian)
+                X, y, w = generate_random(classification, hessian)
                 X .= X * 1e10
                 y .= y * 1e-10
-                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             end
         end
     end
@@ -162,9 +160,9 @@ function run_gradient_tests(Reference_diff, Given_diff, loss_function, activatio
                     num_hidden = rand(30:50)
                     num_features = rand(5:20)
                 end
-                X, y, w = generate_data(num_data, num_features, num_hidden,classification)
+                X, y, w = generate_data(num_data, num_features, num_hidden, classification)
 
-                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+                check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol)
             end
         end
     end
@@ -172,7 +170,7 @@ function run_gradient_tests(Reference_diff, Given_diff, loss_function, activatio
     @testset "$name with sparse weights" begin # Fail for IfElseReverse
         # 7. Random tests with sparse weights
         for _ in 1:num_repeats
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             mask = rand(length(w)) .> 0.7
             offset = 0
             for i in eachindex(w.components)
@@ -182,35 +180,37 @@ function run_gradient_tests(Reference_diff, Given_diff, loss_function, activatio
                 w.components[i] .= map(x -> x == 0.0 && signbit(x) ? 0.0 : x, w.components[i])
                 offset += n
             end # 70% of weights sont zero et -0.0 remplacés par 0.0
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian, broken)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, zero_test)
         end
     end
     @testset "$name with zero matrix" begin
         for _ in 1:num_repeats
             # 8. Random tests with zero matrix
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             for i in eachindex(w.components)
                 w.components[i] .= 0.0
             end
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian, broken)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, zero_test)
 
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             X .= 0.0
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, zero_test)
 
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             y .= 0.0
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, zero_test)
 
-            X, y, w = generate_random(classification,hessian)
+            X, y, w = generate_random(classification, hessian)
             X .= 0.0
             y .= 0.0
-            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, hessian)
+            check_case(X, y, w, loss_function, activation, Reference_diff, Given_diff, rtol, zero_test)
         end
     end  
 end
 
-function run_gradient_tests(Reference_diff, Given_diff; kws...)
+# We default to `test_zero_relu=false`, passing the tests with `test_zero_relu=true`
+# is out of the scope of this project.
+function run_gradient_tests(Reference_diff, Given_diff, test_zero_relu=false; kws...)
     # 1) Identity activation
     run_gradient_tests(Reference_diff, Given_diff, mse, identity_activation, name="Identity"; kws...)
 
@@ -218,9 +218,9 @@ function run_gradient_tests(Reference_diff, Given_diff; kws...)
     run_gradient_tests(Reference_diff, Given_diff, mse, tanh_activation, name="Tanh"; kws...)
 
     # 3) ReLU
-    run_gradient_tests(Reference_diff, Given_diff, mse, relu_activation, name="ReLU"; kws...)
+    run_gradient_tests(Reference_diff, Given_diff, mse, relu_activation, name="ReLU", zero_test=test_zero_relu; kws...)
 
     # 4) Softmax
-    run_gradient_tests(Reference_diff, Given_diff, mse, relu_activation, name="Softmax"; classification = true, kws...)
+    run_gradient_tests(Reference_diff, Given_diff, cross_entropy, relu_softmax, name="Softmax", zero_test=test_zero_relu; classification = true, kws...)
     return
 end
